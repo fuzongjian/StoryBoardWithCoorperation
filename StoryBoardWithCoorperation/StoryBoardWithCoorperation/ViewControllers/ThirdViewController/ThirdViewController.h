6794971b2e0f2ad4e174644ba218ae5111a555df
fuzongjian <2917066078@qq.com>
2016-04-15 16:43:19 +0800

    不需要写任何代码完成配置

---
+++ StoryBoardWithCoorperation/StoryBoardWithCoorperation/ViewControllers/ThirdViewController/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  StoryBoardWithCoorperation
+//
+//  Created by 陈舒澳 on 16/4/15.
+//  Copyright © 2016年 xingbida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@end
